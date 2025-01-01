2765cb6fb3487cfb883b478f1db06c8a235a12ad
guowei <guowei@qq.com>
2025-01-01 11:10:14 +0800

    初始化配置

---
+++ global/setting.go
@@ -0,0 +1,11 @@
+package global
+
+import "github.com/go-programming-tour-book/blog_service/pkg/setting"
+
+//  包全局变量
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/go-programming-tour-book/blog_service/global"
 	"github.com/go-programming-tour-book/blog_service/internal/routers"
+	"github.com/go-programming-tour-book/blog_service/pkg/setting"
 )
+
+// 程序执行顺序： 全局变量初始化--> init方法 --> main方法
+//  不要滥用 init方法， 如果init方法过多， 则很容易迷失在各个库的init方法
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
 
 func main() {
 	router := routers.NewRouter()
@@ -17,4 +30,28 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
