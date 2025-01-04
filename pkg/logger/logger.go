37e9de382fcb6f9a62977888feccdeb7226be03a
guowei <guowei@qq.com>
2025-01-04 23:51:36 +0800

    编写日志组件

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,5 +1,11 @@
 package logger
 
+import (
+	"context"
+	"io"
+	"log"
+)
+
 type Fields map[string]interface{}
 
 const (
@@ -27,4 +33,77 @@ func (l Level) String() string {
 		return "panic"
 	}
 	return ""
+}
+
+type Logger struct {
+	newLogger *log.Logger
+	ctx       context.Context
+	fields    Fields
+	callers   []string
+}
+
+func NewLogger(w io.Writer, prefix string, flag int) *Logger {
+	l := log.New(w, prefix, flag)
+	return &Logger{newLogger: l}
+}
+
+
+func (l *Logger) clone() *Logger {
+	nl := *l
+	return &nl
+}
+
+// 设置日志公共字段
+
+func (l *Logger) WithFields(f Fields) *Logger {
+	ll := l.clone()
+	if ll.fields == nil {
+		ll.fields = make(Fields)
+	}
+	for k, v := range f {
+		ll.fields[k] = v
+	}
+	return ll
+}
+
+// 设置日志上下文属性
+
+func (l *Logger) WithContext(ctx context.Context) *Logger {
+	ll := l.clone()
+	ll.ctx = ctx
+	return ll
+}
+
+// 设置当前某一层调用栈的信息（程序计数器，文件信息和行号）
+
+func (l *Logger) WithCaller(skip int) *Logger {
+	ll := l.clone()
+	pc, file, line, ok := runtime.Caller(skip)
+	if ok {
+		f := runtime.FuncForPC(pc)
+		ll.callers = []string{fmt.Sprintf("%s: %d %s", file, line, f.Name())}
+	}
+	return ll
+}
+
+// 设置当前的整个调用栈信息
+
+func (l *Logger) WithCallersFrames() *Logger {
+	maxCallersDepth := 25
+	minCallersDepth := 10
+	callers := []string{}
+	pcs := make([]uintptr, maxCallersDepth)
+	depth := runtime.Callers(minCallersDepth, pcs)
+	frames := runtime.CallersFrames(pcs[:depth])
+	for frame, more := frames.Next(); more; frame; more = frames.Next() {
+		s := fmt.Sprintf("%s: %d %s", frame.File, frame.Line, frame.Function)
+		callers = append(callers, s)
+		if !more {
+			break
+		}
+
+	}
+	ll := l.clone()
+	ll.callers = callers
+	return ll
 }
