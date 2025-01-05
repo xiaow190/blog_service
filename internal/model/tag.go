4a52cb889cb0d48a64a3c471287df13d18de3dde
guowei <guowei@qq.com>
2025-01-05 22:37:14 +0800

    新建model方法

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/go-programming-tour-book/blog_service/pkg/app"
+import (
+	"github.com/go-programming-tour-book/blog_service/pkg/app"
+	"github.com/jinzhu/gorm"
+)
 
 type Tag struct {
 	*Model
@@ -15,4 +18,56 @@ type TagSwagger struct {
 
 func (t Tag) TableName() string {
 	return "blog_tag"
+}
+
+// model 方法
+// Where 设置筛选条件
+// Offset 偏移量， 用于指定开始返回记录之前要跳过的记录数
+// Limit: 限制检索的记录数
+// Find: 查找符合筛选条件的记录
+// Updates： 更新所选字段
+// Delete: 删除数据
+// Count：统计行为，用于统计模型的记录数
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB) error {
+	db = db.Model(&Tag{}).Where("id = ? AND is_del = ?", t.ID, 0)
+	return db.Update(t).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", t.Model.ID, 0).Delete(&t).Error
 }
