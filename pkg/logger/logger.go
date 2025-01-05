47f2edb59a458712374b1f896f5073be1d8c6647
guowei <guowei@qq.com>
2025-01-05 12:51:19 +0800

    新增日志输出

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,9 +2,15 @@ package logger
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"runtime"
+	"time"
 )
+
+type Level int8
 
 type Fields map[string]interface{}
 
@@ -46,7 +52,6 @@ func NewLogger(w io.Writer, prefix string, flag int) *Logger {
 	l := log.New(w, prefix, flag)
 	return &Logger{newLogger: l}
 }
-
 
 func (l *Logger) clone() *Logger {
 	nl := *l
@@ -95,7 +100,7 @@ func (l *Logger) WithCallersFrames() *Logger {
 	pcs := make([]uintptr, maxCallersDepth)
 	depth := runtime.Callers(minCallersDepth, pcs)
 	frames := runtime.CallersFrames(pcs[:depth])
-	for frame, more := frames.Next(); more; frame; more = frames.Next() {
+	for frame, more := frames.Next(); more; frame, more = frames.Next() {
 		s := fmt.Sprintf("%s: %d %s", frame.File, frame.Line, frame.Function)
 		callers = append(callers, s)
 		if !more {
@@ -106,4 +111,90 @@ func (l *Logger) WithCallersFrames() *Logger {
 	ll := l.clone()
 	ll.callers = callers
 	return ll
+}
+
+// 日志格式化
+func (l *Logger) JSONFormat(level Level, message string) map[string]interface{} {
+	data := make(Fields, len(l.fields)+4)
+	data["level"] = level.String()
+	data["time"] = time.Now().Local().UnixNano()
+	data["message"] = message
+	data["callers"] = l.callers
+	if len(l.fields) > 0 {
+		for k, v := range l.fields {
+			if _, ok := data[k]; !ok {
+				data[k] = v
+			}
+		}
+	}
+	return data
+}
+
+// 日志输出
+func (l *Logger) Output(level Level, message string) {
+	body, _ := json.Marshal(l.JSONFormat(level, message))
+	content := string(body)
+	switch level {
+	case LevelDebug:
+		l.newLogger.Print(content)
+	case LevelInfo:
+		l.newLogger.Print(content)
+	case LevelWarn:
+		l.newLogger.Print(content)
+	case LevelError:
+		l.newLogger.Print(content)
+	case LevelFatal:
+		l.newLogger.Print(content)
+	case LevelPanic:
+		l.newLogger.Print(content)
+	}
+}
+
+// 日志分级输出
+func (l *Logger) Debug(v ...interface{}) {
+	l.Output(LevelDebug, fmt.Sprint(v...))
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.Output(LevelDebug, fmt.Sprintf(format, v...))
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	l.Output(LevelInfo, fmt.Sprint(v...))
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.Output(LevelInfo, fmt.Sprintf(format, v...))
+}
+
+func (l *Logger) Warn(v ...interface{}) {
+	l.Output(LevelWarn, fmt.Sprint(v...))
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	l.Output(LevelWarn, fmt.Sprintf(format, v...))
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	l.Output(LevelFatal, fmt.Sprint(v...))
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	l.Output(LevelFatal, fmt.Sprintf(format, v...))
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	l.Output(LevelError, fmt.Sprint(v...))
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.Output(LevelError, fmt.Sprintf(format, v...))
+}
+
+func (l *Logger) Panic(v ...interface{}) {
+	l.Output(LevelPanic, fmt.Sprint(v...))
+}
+
+func (l *Logger) Panicf(format string, v ...interface{}) {
+	l.Output(LevelPanic, fmt.Sprintf(format, v...))
 }
