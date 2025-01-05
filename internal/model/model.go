a5e090eddefe1677947b7250608c93fcb5f9d7da
guowei <guowei@qq.com>
2025-01-05 23:12:10 +0800

    新增行为的回调

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-programming-tour-book/blog_service/global"
 	"github.com/go-programming-tour-book/blog_service/pkg/setting"
@@ -42,3 +43,26 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
 	return db, nil
 }
+
+// 通过调用 scope.FieldsByName 方法， 获取当前是否包含所需的字段
+// 通过判断 Field.IsBlank 的值，可以得知该字段的值是否为空
+// 若为空， 则调用Fields.Set 方法给该字段设置值。入参类型为 interface{}
+
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		if createTimeField, ok := scope.FieldByName("CreatedOn"); ok {
+			if createTimeField.IsBlank {
+				_ = createTimeField.Set(nowTime)
+			}
+		}
+		if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
+			if modifyTimeField.IsBlank {
+				_ = modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+func UpdateTimeStampForUpdateCallback(scope *gorm.Scope)
+func deleteCallback(scope *gorm.Scope)
+func addExtraSpaceIfExist(str string) string
